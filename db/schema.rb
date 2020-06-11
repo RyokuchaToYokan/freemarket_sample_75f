58b5ab611430ab6403472aadefeecb703a7634f2
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-06-11 18:29:24 +0900

    edit clumn of phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_032140) do
+ActiveRecord::Schema.define(version: 2020_06_11_092440) do
 
   create_table "addresses", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_032140) do
     t.string "city", null: false
     t.string "block", null: false
     t.string "building"
-    t.integer "phone"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
@@ -86,8 +86,8 @@ ActiveRecord::Schema.define(version: 2020_06_08_032140) do
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status_id"
     t.string "size"
+    t.integer "status_id"
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
